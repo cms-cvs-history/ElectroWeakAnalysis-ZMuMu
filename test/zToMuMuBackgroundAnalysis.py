ad9010d395a6f3f4ef10864f1da2148c09ceafea
cmsbuild <>
2009-02-13 23:50:48 +0000

    This commit was manufactured by cvs2git to create tag 'ElectroWeakAnalysis- ZMuMu-CMSSW_3_4_0_pre4'.
    Sprout from master 2009-02-13 23:49:59 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Specify std::exception explicitly (despite the "using std") to avoid'
    Cherrypick from master 2008-06-04 10:09:41 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added config file':
        plugins/BuildFile
        plugins/ZMuMuEfficiency.cc
        plugins/ZToMuMuHistogrammer.cc
        plugins/ZToMuMuIsolationSelector.cc
        test/csa08ZFit.txt
        test/zToMuMuBackgroundAnalysis.py
    Cherrypick from master 2008-07-02 10:40:56 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Change from "root" dependency to "rootgraphics".':
        bin/BuildFile
    Delete:
        bin/csa08HLTFit.cpp
        bin/csa08IsoBkg.cpp
        bin/csa08NewZFit.cpp
        bin/toyMonteCarlo.cpp
        bin/zChi2Fit.cpp
        bin/zFitToyMc.cpp
        bin/zUMLChi2Fit.cpp
        plugins/DimuonMCMatcher.cc
        plugins/DimuonStatistics.cc
        plugins/MCAcceptanceAnalyzer.cc
        plugins/ZGlobalVsSAIsolationAnalyzer.cc
        plugins/ZHLTMatchFilter.cc
        plugins/ZMuMuIsolationAnalyzer.cc
        plugins/ZMuMuPerformances.cc
        plugins/ZMuMuSaMassHistogram.cc
        plugins/ZMuMu_MCanalyzer.cc
        plugins/ZMuMu_vtxAnalyzer.cc
        plugins/ZToMuMuIsoDepositSelector.cc
        plugins/dimuonHLTFilterCheck.cc
        plugins/triggerEfficiency.cc
        test/BuildFile
        test/ZMuMuEfficiency_1.cfg
        test/ZMuMuPerformances.cfg
        test/ZMuMu_MCanalysis.py
        test/create_tree_for_toyMC.C
        test/csa08HLTFit.txt
        test/csa08IsoBkg.txt
        test/csa08NewZFit.txt
        test/debugZMC.cfg
        test/dimuonHLTFilterCheck.cfg
        test/dimuonStatistics.py
        test/errorMatrix.cpp
        test/isoSAEff.py
        test/isoStudy.C
        test/mcAcceptance.cfg
        test/merge.py
        test/pulls.C
        test/start_toy.csh
        test/triggerEfficiency.py
        test/zChi2Fit.txt
        test/zMuMuIsolationAnalysis.py
        test/zToMuMuAnalysis_IsoDeposit.py
        test/zToMuMuAnalysis_zmm.py
        test/zToMuMuHistos.py
        test/zToMuMuHistosFromSubSkim.py
        test/zToMuMuMicroSkim.py
        test/zToMuMuSubSkim.py
        test/zUMLChi2Fit.txt

--- test/zToMuMuBackgroundAnalysis.py
+++ test/zToMuMuBackgroundAnalysis.py
@@ -16,20 +16,24 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-    "file:Wmunu.root"
+    "file:/data1/cmsdata/dimuons/dimuons_wmunu_job1.root",
+    "file:/data1/cmsdata/dimuons/dimuons_wmunu_job2.root",
+    "file:/data1/cmsdata/dimuons/dimuons_wmunu_job3.root"
     )
 )
 
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string("wMuNu.root")
+    fileName = cms.string("dimuons_wmumu.root")
 )
 
 zSelection = cms.PSet(
     cut = cms.string("charge = 0 & daughter(0).pt > 20 & daughter(1).pt > 20 & abs(daughter(0).eta)<2 & abs(daughter(1).eta)<2 & mass > 20"),
     isoCut = cms.double(3.0),
-    isolationType = cms.string("track")
+    muonIsolations1 = cms.InputTag("muonIsolations"),  
+    muonIsolations2 = cms.InputTag("muonIsolations")  
 )
+
 
 process.goodZToMuMu = cms.EDFilter(
     "ZToMuMuIsolatedSelector",
